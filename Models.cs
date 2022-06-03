630a44c9ffb57b30fe57ef67d9ef39e57d2f2444
Sahadeb Patro <sahadebpatro@microsoft.com>
2022-06-03 21:14:16 +0530

    First Publish

---
+++ Models.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleFunctionApp
+{
+   
+        public class TaskModel
+        {
+            public int Id { get; set; }
+            public string CreatedOn { get; set; }
+            public string Description { get; set; }
+            public string IsDone { get; set; }
+        }
+        public class CreateTaskModel
+        {
+            public int Id { get; set; }
+            public string CreatedOn { get; set; }
+            public string Description { get; set; }
+            public string IsDone { get; set; }
+    }
+        public class UpdateTaskModel
+        {
+            public string Description { get; set; }
+            public string IsDone { get; set; }
+        }
+   
+}
